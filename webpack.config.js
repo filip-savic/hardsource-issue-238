c3bbab1eddeca504e5b9635a9b3fcaab59f5fa47
Filip <fsavic@gmail.com>
2018-01-10 23:26:53 +0100

    Error is reproducable on this commit
    1. npm run build to generate initial cache
    2. change css file
    3. npm run build

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: "./style.src.css",
+    output: {
+        path: __dirname,
+        filename: "style.dist.css"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: (loader) => [
+                                require('autoprefixer')()
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HardSourceWebpackPlugin({
+        cacheDirectory: 'node_modules/.cache/hard-source/[confighash]',
+        recordsPath: 'node_modules/.cache/hard-source/[confighash]/records.json',
+        configHash: function(webpackConfig) {
+            return require('node-object-hash')({sort: false}).hash(webpackConfig);
+        },
+        environmentHash: {
+            root: process.cwd(),
+            directories: [],
+            files: ['package-lock.json', 'yarn.lock'],
+            },
+        })
+    ]
+};
